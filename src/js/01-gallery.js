6b6a9f2cd892a866eb880340efdf34473665dcbe
Yuriy Chernyakov <nitro_06@ukr.net>
2023-01-15 17:42:03 +0200

    upd5

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const picturesAll = document.querySelector('.gallery')
+
+const createNewGallery = (elements) => {
+    return elements.map(({preview, original, description}) => {
+        const newDiv = 
+        `<div>
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"/>
+        </a>
+        </div>`;
+        picturesAll.insertAdjacentHTML(`afterbegin`, newDiv);
+    })
+}
+createNewGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt',captionDelay:250})
