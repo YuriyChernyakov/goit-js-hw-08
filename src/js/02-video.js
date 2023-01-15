f4390d936d2b9bc132a744b833b94e027fb906b9
Yuriy Chernyakov <nitro_06@ukr.net>
2023-01-15 20:41:49 +0200

    upd6

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const currentTime = 'videoplayer-current-time';
+let parsedTime;
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(currentTime, seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(currentTime));
+
+function setCurrentTime() {
+  if (localStorage.getItem(currentTime)) {
+    parsedTime = JSON.parse(localStorage.getItem(currentTime));
+    player.setCurrentTime(parsedTime);
+  } else {
+    parsedTime = 0;
+    player.setCurrentTime(parsedTime);
+  }
+}
