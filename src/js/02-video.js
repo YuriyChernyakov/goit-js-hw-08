b9f2580d6049ead2af39adfb23e331c22f281e14
Yuriy Chernyakov <nitro_06@ukr.net>
2023-01-16 18:46:27 +0200

    final v1.0

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,12 +3,12 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-const currentTime = 'videoplayer-current-time';
+const CURRENT_TIME = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
-  localStorage.setItem(currentTime, seconds);
+  localStorage.setItem(CURRENT_TIME, seconds);
 }
 
-player.setCurrentTime(localStorage.getItem(currentTime));
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+const throttle = require('lodash.throttle');
+const feedback = document.querySelector('.feedback-form');
+const CURRENT_DATA = 'feedback-form-state';
+const localstData = JSON.parse(localStorage.getItem(CURRENT_DATA));
+const localstForm = {};
 
+const onInput = event => {
+    localstForm [event.target.name] = event.target.value;
+    localStorage.setItem(CURRENT_DATA, JSON.stringify(localstForm ));
+};
+
+feedback.addEventListener('input', throttle(onInput, 500));
+
+const fillForm = () => {
+    localstData&&localstData.email ? feedback.email.value = localstData.email: null;
+      localstData&&localstData.message ? feedback.message.value = localstData.message: null;
+}
+
+fillForm();
+
+const submitClick = event => {
+    event.preventDefault();
+    event.currentTarget.reset();
+    console.log('email', feedback.email.value);
+    console.log('message:', feedback.message.value);
+}
+
+feedback.addEventListener('submit', submitClick)
