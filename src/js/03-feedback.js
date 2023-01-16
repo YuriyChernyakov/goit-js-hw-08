a20f8ba3e0afc1d7dc7d578a8a1feff3550fff3c
Yuriy Chernyakov <nitro_06@ukr.net>
2023-01-16 19:54:13 +0200

    upd9

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,7 +13,7 @@ feedback.addEventListener('input', throttle(onInput, 500));
 
 const fillForm = () => {
     localstData && localstData.email ? feedback.email.value = localstData.email: null;
-      localstData && localstData.message ? feedback.message.value = localstData.message: null;
+    localstData && localstData.message ? feedback.message.value = localstData.message: null;
 }
 
 fillForm();
