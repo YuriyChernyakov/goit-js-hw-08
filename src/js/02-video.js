2dbc5d7c75791a18ba5a386cc5c337e5d57f7417
Yuriy Chernyakov <nitro_06@ukr.net>
2023-01-19 17:35:32 +0200

    final v1.1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,6 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 const CURRENT_TIME = 'videoplayer-current-time';
-let parsTime;
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
@@ -12,17 +11,6 @@ function onPlay({ seconds }) {
   localStorage.setItem(CURRENT_TIME, seconds);
 }
 
-player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
-setCurrentTime();
-
-function setCurrentTime() {
-  if (localStorage.getItem(CURRENT_TIME)) {
-    parsTime = JSON.parse(localStorage.getItem(CURRENT_TIME));
-    player.setCurrentTime(parsTime);
-  } else {
-    parsTime = 0;
-    player.setCurrentTime(parsTime);
-  }
-}
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
 
 
